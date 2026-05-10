f1854a4372cbbe2be394fd47e06eafc854a11bad
mohamedlotfi030 <mohamedlotfi030@gmail.com>
2026-05-11 02:51:51 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,23 +1,30 @@
 const Products = {
-    tshirt: {
+    'tshirt': {
         id: "tshirt",
         name: "تيشيرت قطن فاخر",
-        images: ["assets/images/tshirt-1.png"],
+        images: ["tshirt-main.png"], // بما أنها في المسار الرئيسي الآن
         options: {
             sizes: ["S", "M", "L", "XL", "XXL"],
-            types: ["طباعة DTF", "تطريز كمبيوتر", "فيلكس"]
-        },
-        basePrice: 150 // اختياري
+            types: ["طباعة DTF", "تطريز"]
+        }
     },
-    rollup: {
+    'rollup': {
         id: "rollup",
         name: "رول أب ستاند",
-        images: ["assets/images/rollup.png"],
+        images: ["rollup-1.jpg"],
         options: {
             sizes: ["80×200 سم", "100×200 سم"],
-            types: ["مستورد ثقيل", "محلي"]
-        },
-        basePrice: 450
+            types: ["مستورد ثقيل", "فاخر"]
+        }
     },
-    // أضف باقي المنتجات بنفس النمط...
+    'bcard': {
+        id: "bcard",
+        name: "كروت شخصية",
+        images: ["business-card-1.jpg"],
+        options: {
+            sizes: ["9x5 سم"],
+            types: ["كوشيه مط", "سلوفان"]
+        }
+    }
+    // يمكنك إضافة باقي المنتجات بنفس الطريقة
 };
