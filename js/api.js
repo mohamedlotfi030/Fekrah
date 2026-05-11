e8c2f5832918600445e043cffc02c57ebd48ffaa
mohamedlotfi030 <mohamedlotfi030@gmail.com>
2026-05-11 04:01:42 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ const API = {
     GOOGLE_SHEET_URL: "https://script.google.com/macros/s/AKfycbxDGCUYlIeIFROBJLV9un3DcAj7jI6FEB_9wFBb4U_fF-Nni9zBIjUtyAC85a5fCcHd/exec",
 
     async sendOrder(customerName, customerPhone) {
-        if (Cart.items.length === 0) return alert("السلة فارغة!");
+        if (Cart.items.length === 0) return;
 
         const orderData = {
             customer: customerName,
@@ -12,7 +12,7 @@ const API = {
             date: new Date().toLocaleString('ar-EG')
         };
 
-        // إرسال البيانات لجوجل شيت
+        // إرسال البيانات لجوجل شيت في الخلفية
         fetch(this.GOOGLE_SHEET_URL, {
             method: 'POST',
             mode: 'no-cors',
@@ -22,11 +22,11 @@ const API = {
         // رسالة الواتساب
         let msg = `*طلب جديد من الموقع*%0A👤 العميل: ${customerName}%0A📞 الهاتف: ${customerPhone}%0A%0A`;
         Cart.items.forEach((item, i) => {
-            msg += `${i+1}. ${item.name} (${item.size} - ${item.option})%0A`;
+            msg += `${i+1}. *${item.name}*%0Aالمقاس: ${item.size} | النوع: ${item.option}%0A%0A`;
         });
 
         window.open(`https://wa.me/${this.WHATSAPP_NUM}?text=${msg}`, '_blank');
         Cart.clear();
-        window.location.href = 'index.html'; // العودة للرئيسية بعد الطلب
+        window.location.href = 'index.html';
     }
 };
